604e78cae798d6b22e7cf93d1c3a6ea16007fb5d
Iran Mederos <iranmederos@gmail.com>
2023-03-29 19:50:38 -0300

    Update models.py

--- core/authentication/models.py
+++ core/authentication/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 USER_TYPE= (('1','Administrador'),('2','Contador'))
 
+
+class Role(models.Model):
+    name = models.CharField(max_length=10,null=False, blank=False,choices=USER_TYPE,default=0)
+
+
 class CustomUser(AbstractUser):
     user = models.AutoField(null=False, blank=False,primary_key=True, unique=True)
     username =  models.CharField(max_length=50,null=True, blank=True,default=None)
@@ -11,11 +16,9 @@ class CustomUser(AbstractUser):
     last_name =  models.CharField(max_length=50,null=False, blank=False)
     phone_number = models.CharField(max_length=15, unique=False, null=True, blank=True)
     email = models.EmailField(max_length=100, unique=True)
+    rol= models.ForeignKey(Role, blank=False, null=False)
        
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'password']
 
-
-class Role(models.Model):
-    name = models.CharField(max_length=10,null=False, blank=False,choices=USER_TYPE,default=0)
